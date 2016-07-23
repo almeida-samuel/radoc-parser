8c9dce0043eb8b7195b30927329fc6c83ea41ba7
Thiago Durante Pires <thdurante@gmail.com>
2016-07-23 00:08:55 -0300

    implementação do codGrupoPontuacao para ATIVIDADES DE QUALIFICAÇÃO

--- src/main/java/formatters/FormatadorAtividadeDeQualificacao.java
+++ src/main/java/formatters/FormatadorAtividadeDeQualificacao.java
@@ -1,5 +1,9 @@
 package formatters;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+
 public class FormatadorAtividadeDeQualificacao extends FormatadorPadrao {
 
     public FormatadorAtividadeDeQualificacao(String pathResolucao) {
@@ -9,5 +13,23 @@ public class FormatadorAtividadeDeQualificacao extends FormatadorPadrao {
     @Override
     public String obtenhaTipoAtividadeResolucao() {
         return "outras";
+    }
+
+    @Override
+    public String obtenhaCampo(Matcher matcher, String campo) {
+        HashMap<String, Map> anexoIIResolucao = getResolucaoParser().obtenhaAtividadesResolucao();
+        String key = matcher.group(1).toLowerCase().trim();
+
+        String codGrupoPontuacao = "";
+        HashMap<String, Map> mapaAtividade = (HashMap<String, Map>) anexoIIResolucao.get(obtenhaTipoAtividadeResolucao());
+
+        for(String k : mapaAtividade.keySet()) {
+            if(k.equals(key) || k.contains(key)) {
+                codGrupoPontuacao = String.valueOf(mapaAtividade.get(k).get(campo));
+                break;
+            }
+        }
+
+        return codGrupoPontuacao;
     }
 }
