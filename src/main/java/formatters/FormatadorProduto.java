1a01b2fd366dfb39dc792c73d403fbed783381bc
Thiago Durante Pires <thdurante@gmail.com>
2016-07-23 01:07:26 -0300

    implementação do codGrupoPontuacao para PRODUTOS

--- src/main/java/formatters/FormatadorProduto.java
+++ src/main/java/formatters/FormatadorProduto.java
@@ -1,5 +1,9 @@
 package formatters;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+
 public class FormatadorProduto extends FormatadorPadrao {
 
     public FormatadorProduto(String pathResolucao) {
@@ -9,5 +13,23 @@ public class FormatadorProduto extends FormatadorPadrao {
     @Override
     public String obtenhaTipoAtividadeResolucao() {
         return "producaoIntelectual";
+    }
+
+    @Override
+    public String obtenhaCampo(Matcher matcher, String campo) {
+        HashMap<String, Map> anexoIIResolucao = getResolucaoParser().obtenhaAtividadesResolucao();
+        String key = matcher.group(1).toLowerCase().trim();
+
+        String codGrupoPontuacao = "";
+        HashMap<String, Map> mapaAtividade = (HashMap<String, Map>) anexoIIResolucao.get(obtenhaTipoAtividadeResolucao());
+
+        for(String k : mapaAtividade.keySet()) {
+            if(k.equals(key) || k.contains(key)) {
+                codGrupoPontuacao = String.valueOf(mapaAtividade.get(k).get(campo));
+                break;
+            }
+        }
+
+        return codGrupoPontuacao;
     }
 }
